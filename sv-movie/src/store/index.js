d38d009583a777deb70ecd6decbc6fa00a95f6ae
yuta ono <youcana40627@gmail.com>
2021-03-17 18:39:59 +0900

    introduce vuex

--- sv-movie/src/store/index.js
+++ sv-movie/src/store/index.js
@@ -5,6 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    movies: [
+      {
+          id: 1,
+          year: 2020,
+          subject: "新受験生数学1A2B",
+          date: new Date(2021, 2, 20, 19, 50, 0),
+          link: "https://google.com"
+      },
+      {
+          id: 2,
+          year: 2020,
+          subject: "新受験生英文読解",
+          date: new Date(2021, 2, 21, 18, 30, 0),
+          link: "https://www.yahoo.co.jp/"
+      },
+    ]
   },
   mutations: {
   },
